40773dd4610285cefea9136d4cb727715bf9b17e
ollanket <onni.ollanketo98@gmail.com>
2022-01-08 03:50:56 +0200

    Add Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,26 +5,32 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-between flex-wrap bg-teal-100 p-3 rounded-t-md fixed bottom-0 w-screen border border-teal-300">
-      <div className="flex items-center justify-between flex-wrap bg-teal-100">
+    <footer className="flex items-center justify-between flex-wrap bg-teal-100 p-3 rounded-t-md border border-teal-300">
+      <div className="flex items-center justify-between flex-wrap bg-teal-100 w-full">
         <div className="flex items-center flex-shrink-0 text-teal-700 mr-6 text-sm">
           <span>Made with&nbsp;</span>
           <MdFavorite />
           <span>&nbsp;&&nbsp;</span>
           <MdCoffee />
-          <span>&nbsp;using&nbsp;</span>
+          <span className=" hidden sm:block">&nbsp;using&nbsp;</span>
           <Alink url="https://nextjs.org/" text="Next.js" />
-          <span>,&nbsp;</span>
+          <span className=" hidden sm:block">,&nbsp;</span>
           <Alink url="https://tailwindcss.com/" text="tailwindcss" />
-          <span>,&nbsp;</span>
+          <span className=" hidden sm:block">,&nbsp;</span>
           <Alink url="https://fauna.com/" text="Fauna" />
-          <span>&nbsp;&&nbsp;</span>
+          <span className=" hidden sm:block">&nbsp;&&nbsp;</span>
           <Alink url="https://www.omdbapi.com/" text="OMDB API" />
+          <span>.</span>
         </div>
-        <div className="fixed right-4 flex items-center text-teal-700">
+        <div className="flex items-center text-teal-700 place-self-end">
           <FaGithub className="text-black" />
           <span>&nbsp;</span>
-          <Alink url="" text="source" />
+          <a
+            className=" font-semibold underline"
+            href="https://github.com/ollanket/movielist-fauna-next.js-ts"
+          >
+            source
+          </a>
         </div>
       </div>
     </footer>
@@ -34,7 +40,7 @@ const Footer = () => {
 const Alink = ({ url, text }: { url: string; text: string }) => {
   return (
     <>
-      <a className="font-semibold underline" href={url}>
+      <a className="font-semibold underline hidden sm:inline-block" href={url}>
         {text}
       </a>
     </>
