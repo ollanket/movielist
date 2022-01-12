a2b7f25495e8f3229810d3ffd0a0ed3f3820fc31
ollanket <onni.ollanketo98@gmail.com>
2022-01-12 04:58:01 +0200

    Add redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+};
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/login",
+        has: [{ type: "cookie", key: "faunaSecret" }],
+        permanent: false,
+        destination: "/personal",
+      },
+      {
+        source: "/signup",
+        has: [{ type: "cookie", key: "faunaSecret" }],
+        permanent: false,
+        destination: "/personal",
+      },
+      {
+        source: "/",
+        has: [{ type: "cookie", key: "faunaSecret" }],
+        permanent: false,
+        destination: "/personal",
+      },
+    ];
+  },
+};
