560f7f34382ca0bc08b9f29571c0f7def3b6ffea
ollanket <onni.ollanketo98@gmail.com>
2022-02-12 04:07:56 +0200

    Fix loading and delete icons, lower search list lenght on smaller screens

--- components/list/Search.tsx
+++ components/list/Search.tsx
@@ -62,49 +62,50 @@ const Search = ({ controls }: Props) => {
   return (
     <div className="flex flex-grow-0 basis-1/6 z-50">
       <div className="flex flex-col w-full items-center justify-center bg-slate-500">
-        <label
-          htmlFor="search"
-          className="flex  w-9/12 sm:w-6/12 justify-center items-center"
-        >
-          {isLoading ? (
-            <div className="flex absolute w-2/3 sm:w-5/12 px-3 justify-end ml-6 pointer-events-none">
-              <div
-                className={`w-4 h-4 border-b-2 border-teal-700 rounded-full animate-spin`}
-              ></div>
-            </div>
-          ) : (
-            <div className="flex absolute w-2/3 sm:w-5/12 px-3 justify-end ml-6 pointer-events-none">
+        <form className="w-full max-w-sm">
+          <div className="flex items-center border-b border-teal-500 py-2 bg-white border rounded-md text-lg">
+            <input
+              className="appearance-none bg-transparent border-none w-full mr-3 py-1 px-2 leading-tight focus:outline-none"
+              id="search"
+              type="text"
+              autoComplete="off"
+              placeholder="Search..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              onFocus={() => setVisible(true)}
+              onBlur={() => setVisible(false)}
+            />
+            {isLoading ? (
+              <div className="flex-shrink-0 py-1 px-2 pr-3">
+                <div
+                  className={`w-4 h-4 border-b-2 border-teal-700 rounded-full animate-spin`}
+                ></div>
+              </div>
+            ) : (
               <button
-                className={` pointer-events-auto w-4 h-4`}
+                className="flex-shrink-0 border-transparent pr-3 text-teal-800 hover:text-black text-md py-1 px-2 rounded"
+                type="button"
                 onClick={() => setSearch("")}
               >
                 <MdClear />
               </button>
-            </div>
-          )}
-          <input
-            id="search"
-            type="search"
-            autoComplete="off"
-            placeholder="Search..."
-            className="shadow appearance-none border rounded w-full py-2 px-3 p-1 font-medium text-lg leading-tight focus:outline-none focus:shadow-outline"
-            onChange={(e) => setSearch(e.target.value)}
-            onFocus={() => setVisible(true)}
-            onBlur={() => setVisible(false)}
-          ></input>
-        </label>
+            )}
+          </div>
+        </form>
         {items && visible && (
           <div className="flex flex-col overflow-visible invisible h-0 w-full sm:w-2/3">
-            <div className="flex flex-col bg-white visible rounded-md p-1 sm:p-2 border border-teal-300 min-h-min mt-1">
+            <div className="flex flex-col bg-white visible rounded-md p-1 sm:p-2 border border-teal-300 mt-1">
               {items.map(({ Title, Year, Poster, imdbID }, i) => (
                 <div
-                  className=" cursor-pointer flex items-center border-t bg-white visible p-0.5 rounded-md hover:shadow-sm  transition ease-in-out delay-75 hover:scale-110 hover:border-0 hover:p-1 hover:bg-slate-100 duration-150"
+                  className={`cursor-pointer flex md:flex items-center border-t bg-white ${
+                    i < 5 ? "visible" : "hidden"
+                  } md:visible p-0.5 rounded-md hover:shadow-sm  transition ease-in-out delay-75 hover:scale-110 hover:border-0 hover:p-1 hover:bg-slate-100 duration-150 `}
                   onClick={() => setVisible(false)}
                   key={i}
                 >
                   <div className="basis-2/12 sm:basis-1/12 h-16 sm:h-20">
                     <img
-                      src={Poster}
+                      src={Poster === "N/A" ? "" : Poster}
                       alt="no img"
                       className="object-fill h-full w-full"
                     ></img>
